9786798496330a9f006e7d2f3f7efdaa3fbcc2bd
Nzmonty <156123804+Nzmonty@users.noreply.github.com>
2025-02-18 19:50:01 -0500

    Added Button - Practice time

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Button } from './Button'
+import { Button } from './Button';
+import './HeroSection.css';
+import '../App.css';
 
 
 function HeroSection() {
@@ -11,6 +13,9 @@ function HeroSection() {
         <div className="hero-btns">
             <Button className='btns' buttonStyle='btn--outline' 
             buttonSize={'btn--large'}>GET STARTED</Button>
+
+            <Button className='btns' buttonStyle='btn--primary' 
+            buttonSize={'btn--large'}>INVEST NOW <i className='far fa-play-circle' /></Button>
         </div>
     </div>
     )
---
+++ src/components/pages/Home.js
@@ -0,0 +1,8 @@
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+function Home () {
+    return (
+        <></>
+    )
+}
