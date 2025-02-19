16551042ef76c7848a71ff78acb9ad9ae96a7517
Nzmonty <156123804+Nzmonty@users.noreply.github.com>
2025-02-19 17:26:52 -0500

    Hero Section updates

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,18 +7,23 @@ import '../App.css';
 function HeroSection() {
     return (
     <div className='hero-container'>
-        <video src="/CalliCoinsStorage/video2.mov" autoPlay loop muted />
+        <video src='/CalliCoinsStorage/Video2.mp4' autoPlay loop muted />
         <h1> PROFITS AWAIT </h1>
         <p>Don't miss out...</p>
-        <div className="hero-btns">
-            <Button className='btns' buttonStyle='btn--outline' 
-            buttonSize={'btn--large'}>GET STARTED</Button>
-
-            <Button className='btns' buttonStyle='btn--primary' 
-            buttonSize={'btn--large'}>INVEST NOW <i className='far fa-play-circle' /></Button>
+        <div className='hero-btns'>
+            <Button className='btns' 
+            buttonStyle='btn--outline' 
+            buttonSize='btn--large'>
+                GET STARTED</Button>
+S
+            <Button className='btns' 
+            buttonStyle='btn--primary' 
+            buttonSize='btn--large'>
+                INVEST NOW 
+                <i className='far fa-play-circle' /></Button>
         </div>
     </div>
     )
 }
 
-export default HeroSection
+export default HeroSection;
